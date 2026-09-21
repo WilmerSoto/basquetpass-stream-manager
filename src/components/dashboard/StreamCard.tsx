372fc7886582d65602d2eaa9be2ed01a98485cb8
Wilmer Soto <wsoto04@gmail.com>
2026-09-21 16:36:32 -0500

    feat: enhance TimezoneDisplay to handle string dates and update StreamCard to utilize the improved component

--- src/components/dashboard/StreamCard.tsx
+++ src/components/dashboard/StreamCard.tsx
@@ -11,6 +11,7 @@ import {
 import StatusBadge from "@/components/common/StatusBadge";
 import { calculateStreamStatus } from "@/utils/streamUtils";
 import { AR, CO } from "country-flag-icons/react/3x2";
+import TimezoneDisplay from "@/components/common/TimezoneDisplay";
 
 interface StreamCardProps {
   stream: Stream;
@@ -37,14 +38,7 @@ export default function StreamCard({
         <CardTitle className="flex flex-col gap-2 border-b">
           <div className="flex justify-between">
             <StatusBadge variant="card" type={currentStatus} />
-            <div className="flex gap-2">
-              <div className="flex items-center justify-center gap-1.5">
-                <AR className="size-4" /> XD
-              </div>
-              <div className="flex items-center justify-center gap-1.5">
-                <CO className="size-4" /> XD
-              </div>
-            </div>
+            <TimezoneDisplay variant="card" date={stream.startTime} />
           </div>
           <div className="flex gap-4">
             <h1 className="text-lg font-semibold text-white">
